1c1592d38b06874f60dde8582fde69a0729efda2
Ed <eberne@att.net>
2016-07-16 10:03:10 -0700

    Merging branch into master

--- Vogella Tutorial/src/first/GitTest.java
+++ Vogella Tutorial/src/first/GitTest.java
@@ -3,7 +3,9 @@ package first;
 public class GitTest {
 
 	public static void main(String[] args) {
-	System.out.println("Git is fun");
+	System.out.println("Git is cool");
 	System.out.println("First revision");
+	System.out.println("I hope this is on the branch");
+	System.out.println("Branch worked fine. Merge into master");
 	}
 }
